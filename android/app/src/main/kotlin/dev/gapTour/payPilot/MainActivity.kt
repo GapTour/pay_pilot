50e86aa4b1959f839ac3a906c447d33aa0e685af
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-08 01:15:21 +0330

    feat: update icon launcher and app name

---
+++ android/app/src/main/kotlin/dev/gapTour/payPilot/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.gapTour.payPilot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "pay_pilot");
+    gtk_header_bar_set_title(header_bar, "Pay Pilot DEV");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "pay_pilot");
+    gtk_window_set_title(window, "Pay Pilot DEV");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
