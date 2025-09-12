93eda07358a4ed672699d839a7326ec70c490356
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-12 21:48:48 +0330

    feat: implement teams table

---
+++ lib/core/database/tables/teams.dart
@@ -0,0 +1,8 @@
+import 'package:drift/drift.dart';
+
+class Teams extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text()();
+  TextColumn get description => text().nullable()();
+  DateTimeColumn get createAt => dateTime().withDefault(currentDate)();
+}
