350168edc14dec7b01d846a6507b9ebccfa9e152
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-14 02:35:44 +0330

    feat: implement selecting events and update add report dialog box widget

--- lib/features/reports/presentation/widgets/selecting_events.dart
+++ lib/features/reports/presentation/widgets/selecting_events.dart
@@ -3,27 +3,30 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
-import 'package:pay_pilot/core/database/app_database.dart';
+import 'package:pay_pilot/core/data/models/event_details_model.dart';
+import 'package:pay_pilot/core/database/tables/event_transactions.dart';
 import 'package:pay_pilot/core/utils/helpers/amount_helper.dart';
-import 'package:pay_pilot/features/incomes/presentation/cubit/incomes_cubit.dart';
+import 'package:pay_pilot/core/utils/theme/app_theme.dart';
+import 'package:pay_pilot/features/reports/presentation/cubit/selecting_events_cubit.dart';
 import 'package:pay_pilot/locator.dart';
 
-class SelectingIncomes extends StatefulWidget {
-  final List<Income> selectedIncomes;
+class SelectingEvents extends StatefulWidget {
+  final List<EventDetailsModel> selectedEvents;
   final ({int month, int year})? selectedDate;
-  final Function(Income income, ({int month, int year}) selectedDate) onPressed;
-  const SelectingIncomes({
+  final Function(EventDetailsModel event, ({int month, int year}) selectedDate)
+  onPressed;
+  const SelectingEvents({
     super.key,
-    required this.selectedIncomes,
+    required this.selectedEvents,
     required this.selectedDate,
     required this.onPressed,
   });
 
   @override
-  State<SelectingIncomes> createState() => _SelectingIncomesState();
+  State<SelectingEvents> createState() => _SelectingEventsState();
 }
 
-class _SelectingIncomesState extends State<SelectingIncomes> {
+class _SelectingEventsState extends State<SelectingEvents> {
   late int selectedYear;
   late int selectedMonth;
 
@@ -51,9 +54,9 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
 
   final List<int> years = List.generate(5, (index) => index + 2024);
 
-  List<Income> sortedIncome(List<Income> allIncomes) {
-    if (allIncomes.isEmpty) return [];
-    return allIncomes.where((element) {
+  List<EventDetailsModel> sortedEvents(List<EventDetailsModel> allEvents) {
+    if (allEvents.isEmpty) return [];
+    return allEvents.where((element) {
       return element.date.month == (selectedMonth) &&
           element.date.year == (selectedYear);
     }).toList();
@@ -84,12 +87,13 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => IncomesCubit(locator()),
+      create: (context) => SelectingEventsCubit(locator()),
       child: Builder(
         builder: (context) {
           return Column(
             children: [
               Card(
+                color: kSecondaryColor,
                 child: Padding(
                   padding: const EdgeInsets.only(bottom: 3.0, top: 4),
                   child: Stack(
@@ -121,6 +125,7 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
                                 controller: monthController,
                                 onSelectedItemChanged: (value) {
                                   selectedMonth = value + 1;
+
                                   setState(() {});
                                 },
                                 children: months.entries.map((e) {
@@ -143,6 +148,7 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
                                 }).toList(),
                                 onSelectedItemChanged: (value) {
                                   selectedYear = value + 2024;
+
                                   setState(() {});
                                 },
                               ),
@@ -155,16 +161,16 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
                 ),
               ),
 
-              BlocBuilder<IncomesCubit, IncomesState>(
+              BlocBuilder<SelectingEventsCubit, SelectingEventsState>(
                 builder: (context, state) {
-                  final incomes = sortedIncome(state.incomes);
+                  final List<EventDetailsModel> events = [];
 
                   final isLoading =
-                      state.incomesStatus == IncomesStatus.loading ||
-                      state.incomesStatus == IncomesStatus.initial;
+                      state is SelectingEventsInitial ||
+                      state is SelectingEventsLoading;
 
-                  if (state.incomesStatus == IncomesStatus.initial) {
-                    context.read<IncomesCubit>().loadIncomes();
+                  if (state is SelectingEventsInitial) {
+                    context.read<SelectingEventsCubit>().loadEvents();
                   }
 
                   if (isLoading) {
@@ -173,25 +179,52 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
                     );
                   }
 
-                  if (incomes.isEmpty) {
-                    return Center(
-                      child: Text(
-                        'No income found on ${months[selectedMonth]}, $selectedYear',
+                  if (state is SelectingEventsSuccess) {
+                    events.addAll(sortedEvents(state.events));
+                  }
+
+                  if (state is SelectingEventsSuccess && events.isEmpty) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 18.0),
+                      child: Center(
+                        child: Text(
+                          'No events found on ${months[selectedMonth]}, $selectedYear',
+                        ),
                       ),
                     );
                   }
 
                   return Expanded(
                     child: ListView.separated(
-                      itemCount: incomes.length,
+                      itemCount: events.length,
                       padding: EdgeInsets.symmetric(vertical: 12),
-                      separatorBuilder: (context, index) => Gap(3),
+                      separatorBuilder: (context, index) => Gap(12),
                       itemBuilder: (context, index) {
-                        final isSelected = widget.selectedIncomes.any(
-                          (element) => element.id == incomes[index].id,
+                        final double totalExpense = events[index].transactions
+                            .fold(0, (previousValue, element) {
+                              if (element.transactionType ==
+                                  TransactionType.income) {
+                                return previousValue;
+                              }
+                              return previousValue + element.amount;
+                            });
+                        final double totalIncome = events[index].transactions
+                            .fold(0, (previousValue, element) {
+                              if (element.transactionType ==
+                                  TransactionType.expense) {
+                                return previousValue;
+                              }
+                              return previousValue + element.amount;
+                            });
+                        final isSelected = widget.selectedEvents.any(
+                          (element) => element.id == events[index].id,
                         );
 
-                        return Card(
+                        return DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: BoxBorder.all(color: kOnPrimaryColor),
+                          ),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.start,
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -200,8 +233,9 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
                                 children: [
                                   Checkbox.adaptive(
                                     value: isSelected,
+                                    activeColor: kSecondaryColor,
                                     onChanged: (value) {
-                                      widget.onPressed.call(incomes[index], (
+                                      widget.onPressed.call(events[index], (
                                         month: selectedMonth,
                                         year: selectedYear,
                                       ));
@@ -209,7 +243,7 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
                                   ),
                                   Expanded(
                                     child: Text(
-                                      incomes[index].title,
+                                      events[index].title,
                                       textAlign: TextAlign.left,
                                       style: const TextStyle(fontSize: 18),
                                     ),
@@ -218,25 +252,45 @@ class _SelectingIncomesState extends State<SelectingIncomes> {
                               ),
                               Padding(
                                 padding: const EdgeInsets.only(
-                                  left: 45.0,
-                                  bottom: 8,
+                                  left: 25.0,
+                                  bottom: 12,
                                   right: 18,
                                 ),
-                                child: Row(
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
                                   children: [
-                                    Expanded(
-                                      child: Text(
-                                        AmountHelper.integerToFormattedPrice(
-                                          incomes[index].amount,
-                                        ),
-                                        style: const TextStyle(fontSize: 14),
-                                      ),
-                                    ),
                                     Text(
                                       DateFormat.MMMEd().format(
-                                        incomes[index].date,
+                                        events[index].date,
                                       ),
                                       style: const TextStyle(fontSize: 14),
+                                    ),
+                                    Text.rich(
+                                      TextSpan(
+                                        children: [
+                                          TextSpan(text: 'Income: '),
+                                          TextSpan(
+                                            text:
+                                                AmountHelper.integerToFormattedPrice(
+                                                  totalIncome,
+                                                ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Text.rich(
+                                      TextSpan(
+                                        children: [
+                                          TextSpan(text: 'Expense: '),
+                                          TextSpan(
+                                            text:
+                                                AmountHelper.integerToFormattedPrice(
+                                                  totalExpense,
+                                                ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ],
                                 ),
