4119cbf7e0939002fe043276033e25fccfc6e2d9
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-13 20:30:28 +0330

    ref: remove db service class and add all new repositories on locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,15 +1,27 @@
 import 'package:get_it/get_it.dart';
 import 'package:pay_pilot/core/database/app_database.dart';
+import 'package:pay_pilot/core/database/daos/event_dao/event_dao.dart';
+import 'package:pay_pilot/core/database/daos/member_dao/member_dao.dart';
+import 'package:pay_pilot/core/database/daos/ratio_dao/ratio_dao.dart';
+import 'package:pay_pilot/core/database/daos/report_dao/report_dao.dart';
+import 'package:pay_pilot/core/database/daos/team_dao/team_dao.dart';
 import 'package:pay_pilot/core/database/platform/platfrom.dart';
-import 'package:pay_pilot/core/services/db_service.dart';
-import 'package:pay_pilot/features/incomes/data/income_db_provider.dart';
-import 'package:pay_pilot/features/incomes/repository/income_repository.dart';
+import 'package:pay_pilot/features/event_transactions/data/transaction_db_provider.dart';
+import 'package:pay_pilot/features/event_transactions/repository/transaction_repository.dart';
+import 'package:pay_pilot/features/events/data/event_db_provider.dart';
+import 'package:pay_pilot/features/events/repository/event_repository.dart';
+import 'package:pay_pilot/features/member_details/data/member_details_db_provider.dart';
+import 'package:pay_pilot/features/member_details/repository/member_details_repository.dart';
 import 'package:pay_pilot/features/members/data/member_db_provider.dart';
 import 'package:pay_pilot/features/members/repository/member_repository.dart';
 import 'package:pay_pilot/features/report_details/data/report_details_db_provider.dart';
 import 'package:pay_pilot/features/report_details/repository/report_details_repository.dart';
 import 'package:pay_pilot/features/reports/data/report_db_provider.dart';
 import 'package:pay_pilot/features/reports/repository/report_repository.dart';
+import 'package:pay_pilot/features/team_members/data/team_members_db_provider.dart';
+import 'package:pay_pilot/features/team_members/repository/team_members_repository.dart';
+import 'package:pay_pilot/features/teams/data/teams_db_provider.dart';
+import 'package:pay_pilot/features/teams/repository/team_repository.dart';
 
 GetIt locator = GetIt.instance;
 
@@ -25,18 +37,34 @@ void _callServices() {
     AppDatabase(Platform.createDatabaseConnection('pay_pilot_db')),
   );
 
-  locator.registerSingleton<DatabaseService>(DatabaseService(database));
+  locator.registerSingleton<TeamDao>(TeamDao(database));
+  locator.registerSingleton<ReportDao>(ReportDao(database));
+  locator.registerSingleton<RatioDao>(RatioDao(database));
+  locator.registerSingleton<MemberDao>(MemberDao(database));
+  locator.registerSingleton<EventDao>(EventDao(database));
 }
 
 void _callProviders() {
   locator.registerLazySingleton<MemberDbProvider>(
     () => MemberDbProvider(locator()),
   );
-  locator.registerLazySingleton<IncomeDbProvider>(
-    () => IncomeDbProvider(locator()),
+  locator.registerLazySingleton<MemberDetailsDbProvider>(
+    () => MemberDetailsDbProvider(locator()),
+  );
+  locator.registerLazySingleton<EventDbProvider>(
+    () => EventDbProvider(locator(), locator()),
+  );
+  locator.registerLazySingleton<TransactionDbProvider>(
+    () => TransactionDbProvider(locator()),
+  );
+  locator.registerLazySingleton<TeamsDbProvider>(
+    () => TeamsDbProvider(locator()),
+  );
+  locator.registerLazySingleton<TeamMembersDbProvider>(
+    () => TeamMembersDbProvider(locator(), locator(), locator()),
   );
   locator.registerLazySingleton<ReportDbProvider>(
-    () => ReportDbProvider(locator()),
+    () => ReportDbProvider(locator(), locator()),
   );
   locator.registerLazySingleton<ReportDetailsDbProvider>(
     () => ReportDetailsDbProvider(locator()),
@@ -47,8 +75,20 @@ void _callRepositories() {
   locator.registerLazySingleton<MemberRepository>(
     () => MemberRepository(locator()),
   );
-  locator.registerLazySingleton<IncomeRepository>(
-    () => IncomeRepository(locator()),
+  locator.registerLazySingleton<MemberDetailsRepository>(
+    () => MemberDetailsRepository(locator()),
+  );
+  locator.registerLazySingleton<EventRepository>(
+    () => EventRepository(locator()),
+  );
+  locator.registerLazySingleton<TransactionRepository>(
+    () => TransactionRepository(locator()),
+  );
+  locator.registerLazySingleton<TeamRepository>(
+    () => TeamRepository(locator()),
+  );
+  locator.registerLazySingleton<TeamMembersRepository>(
+    () => TeamMembersRepository(locator()),
   );
   locator.registerLazySingleton<ReportRepository>(
     () => ReportRepository(locator()),
