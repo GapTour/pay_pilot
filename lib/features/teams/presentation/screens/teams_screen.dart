003badd5f5db2212819bffcaa0d82bc740f5bead
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-14 02:34:59 +0330

    feat: implement teams screen

---
+++ lib/features/teams/presentation/screens/teams_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pay_pilot/core/app/app_routes.dart';
+import 'package:pay_pilot/core/utils/constants/app_arguments.dart';
+import 'package:pay_pilot/core/utils/theme/app_theme.dart';
+import 'package:pay_pilot/core/widgets/app_list.dart';
+import 'package:pay_pilot/core/widgets/app_tile.dart';
+import 'package:pay_pilot/features/teams/presentation/cubit/teams_cubit.dart';
+import 'package:pay_pilot/features/teams/presentation/widgets/add_team_dialog_box.dart';
+import 'package:pay_pilot/features/teams/presentation/widgets/edit_team_dialog_box.dart';
+
+class TeamsScreen extends StatefulWidget {
+  static const routeName = '/teams';
+
+  const TeamsScreen({super.key});
+
+  @override
+  State<TeamsScreen> createState() => _TeamsScreenState();
+}
+
+class _TeamsScreenState extends State<TeamsScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    context.read<TeamsCubit>().loadTeams();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Teams')),
+      body: BlocBuilder<TeamsCubit, TeamsState>(
+        builder: (context, state) {
+          final members = state.teams;
+          final isLoading =
+              state.teamsStatus == TeamsStatus.loading ||
+              state.teamsStatus == TeamsStatus.initial;
+
+          if (isLoading) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }
+
+          return AppList(
+            itemCount: members.length,
+            emptyInboxMessage: 'There is no team yet!',
+            itemBuilder: (context, index) {
+              return AppTile(
+                height: 46,
+                previewButtonTitle: 'Team\'s Members',
+                onPreview: () {
+                  context.pushNamed(
+                    AppRoutes.teamMembersScreen,
+                    pathParameters: {
+                      AppArguments.teamDetails: '${members[index].id}',
+                    },
+                  );
+                },
+                onEdit: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return EditTeamDialogBox(
+                        team: members[index],
+                        onPressedSubmit: (team) {
+                          context.read<TeamsCubit>().updateTeam(team);
+                        },
+                      );
+                    },
+                  );
+                },
+                child: Text(
+                  members[index].title,
+                  textAlign: TextAlign.left,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontSize: 18),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (_) {
+              return AddTeamDialogBox(
+                onPressedSubmit: (team) {
+                  context.read<TeamsCubit>().addTeam(team);
+                },
+              );
+            },
+          );
+        },
+        backgroundColor: kSecondaryColor,
+        splashColor: kPrimaryColor,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
