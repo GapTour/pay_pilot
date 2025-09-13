cab368006ade0315077df7e983e3caaf25f72813
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-13 20:24:12 +0330

    feat: implement team cubit and related classes

---
+++ lib/features/teams/data/teams_db_provider.dart
@@ -0,0 +1,21 @@
+import 'package:pay_pilot/core/database/app_database.dart';
+import 'package:pay_pilot/core/database/daos/team_dao/team_dao.dart';
+import 'package:pay_pilot/features/teams/data/team_edit_form.dart';
+import 'package:pay_pilot/features/teams/data/team_form.dart';
+
+class TeamsDbProvider {
+  final TeamDao _dbService;
+  TeamsDbProvider(this._dbService);
+
+  Future<List<Team>> getAllTeams() async {
+    return await _dbService.getAllTeams();
+  }
+
+  Future<int> insertTeam(TeamForm team) async {
+    return await _dbService.insertTeam(team);
+  }
+
+  Future<void> updateTeam(TeamEditForm team) async {
+    await _dbService.updateTeam(team);
+  }
+}
