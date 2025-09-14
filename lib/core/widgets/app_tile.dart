53167bb51d8127c4888aeb13414cd7bdd8e581d2
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-14 04:22:45 +0330

    feat: implement delete button on app tile widget

--- lib/core/widgets/app_tile.dart
+++ lib/core/widgets/app_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pay_pilot/core/utils/theme/app_theme.dart';
 import 'package:pay_pilot/core/widgets/app_elevated_button.dart';
 
@@ -9,6 +10,7 @@ class AppTile extends StatefulWidget {
   final VoidCallback? onPreview;
   final String? previewButtonTitle;
   final VoidCallback? onEdit;
+  final VoidCallback? onDelete;
   final bool isActive;
   const AppTile({
     required this.child,
@@ -17,6 +19,7 @@ class AppTile extends StatefulWidget {
     this.onEdit,
     this.onPreview,
     this.previewButtonTitle,
+    this.onDelete,
     super.key,
   });
 
@@ -58,7 +61,7 @@ class _AppTileState extends State<AppTile> {
         ),
         duration: Duration(milliseconds: 150),
         curve: Curves.linear,
-        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+        // padding: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
         onEnd: () {
           if (!_showButtons && _isExpanded) {
             _showButtons = true;
@@ -70,40 +73,96 @@ class _AppTileState extends State<AppTile> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Gap(5),
-            widget.child,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+              child: widget.child,
+            ),
             Gap(5),
             if (_showButtons) ...[
               Gap(5),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (widget.onPreview != null)
-                    Expanded(
-                      child: AppElevatedButton(
-                        onTap: widget.onPreview,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (widget.onPreview != null)
+                      Expanded(
+                        child: AppElevatedButton(
+                          onTap: widget.onPreview,
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text(
+                              widget.previewButtonTitle ?? 'Preview',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      )
+                    else
+                      Spacer(),
+                    if (widget.onEdit != null)
+                      AppElevatedButton(
+                        onTap: widget.onEdit,
                         child: Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: Text(
-                            widget.previewButtonTitle ?? 'Preview',
-                            textAlign: TextAlign.center,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25.0,
+                            vertical: 5,
+                          ),
+                          child: Text('Edit'),
+                        ),
+                      ),
+                    if (widget.onDelete != null) ...[
+                      AppElevatedButton(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                backgroundColor: kPrimaryColor,
+                                title: Text('Are sure about this action?'),
+                                content: Text(
+                                  'Notice that if you used this item, it effects those and delete all data that related to this item too.',
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      context.pop();
+                                    },
+                                    child: Text('Cancel'),
+                                  ),
+                                  AppElevatedButton(
+                                    onTap: () {
+                                      widget.onDelete!.call();
+                                      context.pop();
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 25.0,
+                                        vertical: 5,
+                                      ),
+                                      child: Text(
+                                        'Delete',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Icon(
+                            Icons.delete_outline_rounded,
+                            color: kErrorColor,
+                            size: 20,
                           ),
                         ),
                       ),
-                    )
-                  else
-                    Spacer(),
-                  if (widget.onEdit != null)
-                    AppElevatedButton(
-                      onTap: widget.onEdit,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 25.0,
-                          vertical: 5,
-                        ),
-                        child: Text('Edit'),
-                      ),
-                    ),
-                ],
+                    ],
+                  ],
+                ),
               ),
             ],
           ],
