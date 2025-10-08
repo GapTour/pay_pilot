9f7e19506c83311070967caf28d71b1b5eafb1eb
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-10-08 16:54:01 +0330

    feat: add EventRatioEditForm model
    Introduces the EventRatioEditForm data model with fields for id, ratio, memberID, and eventID to support event ratio editing functionality.

---
+++ lib/features/event_details/data/models/event_ratio_edit_form.dart
@@ -0,0 +1,13 @@
+class EventRatioEditForm {
+  final int id;
+  final double ratio;
+  final int memberID;
+  final int eventID;
+
+  EventRatioEditForm({
+    required this.id,
+    required this.ratio,
+    required this.memberID,
+    required this.eventID,
+  });
+}
