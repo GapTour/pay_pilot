4784650dc5e1f786116ad630b004885b8f0f1ccd
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-10-10 21:55:20 +0330

    ref: add event details cubit on event transactions list widget

--- lib/features/event_details/presentation/widgets/event_transactions_list.dart
+++ lib/features/event_details/presentation/widgets/event_transactions_list.dart
@@ -10,70 +10,80 @@ import 'package:pay_pilot/features/event_details/presentation/cubit/event_detail
 import 'package:pay_pilot/features/event_details/presentation/widgets/edit_event_transaction_dialog_box.dart';
 
 class EventTransactionsList extends StatelessWidget {
-  final String eventID;
-  final List<TransactionModel> transactions;
-  const EventTransactionsList({
-    required this.eventID,
-    required this.transactions,
-    super.key,
-  });
+  final int eventID;
+  const EventTransactionsList(this.eventID, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppList(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: transactions.length,
-      emptyInboxMessage: 'There is no transaction yet!',
-      itemBuilder: (context, index) {
-        return AppTile(
-          height: 75,
-          onEdit: () {
-            showDialog(
-              context: context,
-              builder: (_) {
-                return EditEventTransactionDialogBox(
-                  transaction: transactions[index],
-                  eventID: int.parse(eventID),
-                  onPressedSubmit: (transaction) {
-                    context.read<EventDetailsCubit>().updateTransaction(
-                      transaction,
+    return BlocBuilder<EventDetailsCubit, EventDetailsState>(
+      buildWhen: (p, c) {
+        if (p.transactions != c.transactions) return true;
+        if (p.eventTabsStatus != c.eventTabsStatus) return true;
+        return false;
+      },
+      builder: (context, state) {
+        final List<TransactionModel> transactions = state.transactions;
+
+        if (state.eventTabsStatus.isLoading) {
+          return const Center(child: CircularProgressIndicator.adaptive());
+        }
+
+        return AppList(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.zero,
+          itemCount: transactions.length,
+          emptyInboxMessage: 'There is no transaction yet!',
+          itemBuilder: (context, index) {
+            return AppTile(
+              height: 75,
+              onEdit: () {
+                showDialog(
+                  context: context,
+                  builder: (_) {
+                    return EditEventTransactionDialogBox(
+                      transaction: transactions[index],
+                      eventID: eventID,
+                      onPressedSubmit: (transaction) {
+                        context.read<EventDetailsCubit>().updateTransaction(
+                          transaction,
+                        );
+                      },
                     );
                   },
                 );
               },
-            );
-          },
-          onDelete: () {
-            context.read<EventDetailsCubit>().deleteTransaction(
-              transactions[index].id,
-            );
-          },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
+              onDelete: () {
+                context.read<EventDetailsCubit>().deleteTransaction(
+                  transactions[index].id,
+                );
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Expanded(
-                    child: Text(
-                      AmountHelper.integerToFormattedPrice(
-                        transactions[index].amount,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          AmountHelper.integerToFormattedPrice(
+                            transactions[index].amount,
+                          ),
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(fontSize: 18),
+                        ),
                       ),
-                      textAlign: TextAlign.left,
-                      style: const TextStyle(fontSize: 18),
-                    ),
+                      Text(transactions[index].transactionType.name),
+                    ],
                   ),
-                  Text(transactions[index].transactionType.name),
+                  Gap(5),
+                  Text(
+                    'on ${DateFormat.MMMEd().format(transactions[index].date)}',
+                    style: const TextStyle(fontSize: 14),
+                  ),
                 ],
               ),
-              Gap(5),
-              Text(
-                'on ${DateFormat.MMMEd().format(transactions[index].date)}',
-                style: const TextStyle(fontSize: 14),
-              ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
