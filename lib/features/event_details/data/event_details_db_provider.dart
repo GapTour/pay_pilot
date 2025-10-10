fffa0d926b531c1c647be4666972e5bd3b0f7d27
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-10-10 21:15:32 +0330

    feat: add get all members and insert ratio methods on event details db provider

--- lib/features/event_details/data/event_details_db_provider.dart
+++ lib/features/event_details/data/event_details_db_provider.dart
@@ -1,12 +1,16 @@
 import 'package:pay_pilot/core/data/models/event_details_model.dart';
+import 'package:pay_pilot/core/database/app_database.dart';
 import 'package:pay_pilot/core/database/daos/event_dao/event_dao.dart';
+import 'package:pay_pilot/core/database/daos/member_dao/member_dao.dart';
 import 'package:pay_pilot/features/event_details/data/models/event_ratio_edit_form.dart';
+import 'package:pay_pilot/features/event_details/data/models/event_ratio_form.dart';
 import 'package:pay_pilot/features/event_details/data/transaction_edit_form.dart';
 import 'package:pay_pilot/features/event_details/data/transaction_form.dart';
 
 class EventDetailsDbProvider {
   final EventDao _dbService;
-  EventDetailsDbProvider(this._dbService);
+  final MemberDao _memberDao;
+  EventDetailsDbProvider(this._dbService, this._memberDao);
 
   Future<EventDetailsModel> getEvent(int id) async {
     return await _dbService.getEventInfo(id);
@@ -14,6 +18,10 @@ class EventDetailsDbProvider {
 
   Future<int> insertTransaction(TransactionForm transaction) async {
     return await _dbService.insertTransaction(transaction);
+  }
+
+  Future<int> insertRatio(EventRatioForm ratio) async {
+    return await _dbService.insertRatio(ratio);
   }
 
   Future<void> updateTransaction(TransactionEditForm transaction) async {
@@ -30,5 +38,9 @@ class EventDetailsDbProvider {
 
   Future<void> deleteEventRatio(int ratioID) async {
     await _dbService.deleteRatio(ratioID);
+  }
+
+  Future<List<Member>> getAllMembers() async {
+    return await _memberDao.getAllMembers();
   }
 }
