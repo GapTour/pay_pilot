66673d4b9b2399f395522e78bd5d28bda1b38fa8
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-12-08 03:57:11 +0330

    feat: add error response model

---
+++ lib/core/data/response/error_response.dart
@@ -0,0 +1,50 @@
+import 'package:dio/dio.dart';
+
+class ErrorResponse {
+  final String message;
+  final String status;
+  final int code;
+  final String data;
+
+  ErrorResponse({
+    required this.message,
+    required this.status,
+    required this.code,
+    required this.data,
+  });
+
+  factory ErrorResponse.fromMap(DioException e) {
+    if ((e.response?.statusCode ?? 1000) > 500 ||
+        e.response?.data is! Map<String, dynamic>) {
+      return ErrorResponse(
+        message: 'default error response',
+        status: 'Failure',
+        code: e.response?.statusCode ?? 520,
+        data: 'Something went wrong!',
+      );
+    }
+
+    final Map<String, dynamic> map = e.response?.data ?? {};
+
+    return ErrorResponse(
+      message: map['message'] ?? '',
+      status: map['status'] ?? '',
+      code: map['code'] ?? 0,
+      data: map['data'] ?? '',
+    );
+  }
+
+  factory ErrorResponse.defaultError(String? message) {
+    return ErrorResponse(
+      message: 'default error response',
+      status: 'Failure',
+      code: 600,
+      data: message ?? 'Something went wrong!',
+    );
+  }
+
+  @override
+  String toString() {
+    return 'ErrorResponse(message: $message, status: $status, code: $code, data: $data)';
+  }
+}
