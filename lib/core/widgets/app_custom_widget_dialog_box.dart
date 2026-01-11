f6f1dd7154ee8772f83321914bf6c975bf38541b
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2026-01-11 21:35:42 +0330

    feat: implement app custom widget dialog box

---
+++ lib/core/widgets/app_custom_widget_dialog_box.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pay_pilot/core/utils/theme/app_theme.dart';
+import 'package:pay_pilot/core/widgets/app_elevated_button.dart';
+
+class AppCustomWidgetDialogBox extends StatelessWidget {
+  final String title;
+  final Widget child;
+  final VoidCallback onPressed;
+  const AppCustomWidgetDialogBox({
+    super.key,
+    required this.title,
+    required this.child,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 780,
+        height: 507,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Stack(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 35), child: child),
+              Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: kPrimaryColor),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      title,
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: kPrimaryColor),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: AppElevatedButton(
+                            onTap: onPressed,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Submit',
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.displayLarge,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Gap(8),
+                        TextButton(
+                          onPressed: () => context.pop(),
+
+                          child: Text(
+                            'Cancel',
+                            style: Theme.of(context).textTheme.displayMedium,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
