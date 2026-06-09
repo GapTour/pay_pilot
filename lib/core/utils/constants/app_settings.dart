08baa3e5bab8e21833e52150dbe14bbb454099ea
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2026-06-09 06:40:49 +0330

    chore: bump version to 2.1.0
    Update version number across Android, iOS, and pubspec configurations.

--- lib/core/utils/constants/app_settings.dart
+++ lib/core/utils/constants/app_settings.dart
@@ -1,5 +1,5 @@
 abstract class AppSettings {
   /// do not forget to update the `pubspec.yaml` version too
-  static const String version = '2.0.1';
+  static const String version = '2.1.0';
   static const bool isInterceptorLogEnabled = true;
 }
