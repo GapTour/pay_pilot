d6132878050a3c7cac4aceab9f0df42b9c98fa37
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2026-06-13 15:42:47 +0330

    chore(release): Bump app version to 2.1.3 and update build codes
    Updates the application's version across all platforms (Android, iOS, Flutter/Dart) from 2.1.2 to 2.1.3. This includes incrementing the Android version code from 5 to 6 to ensure a new build is recognized by app stores.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5
-        versionName = "2.1.2"
+        versionCode = 6
+        versionName = "2.1.3"
     }
 
     buildTypes {
